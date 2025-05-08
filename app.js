36e894ee738a09520e2b0fbbefce1cc5f25fe09b
wambui <wambuistephen@zetech.ac.ke>
2025-05-09 02:17:04 +0300

    updates structure

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-import express from 'express';
-const bodyParser = required ('body-parser');
+const express = require ('express');
+const bodyParser = require ('body-parser');
 
 const app = express();
 
 // body-parser middleware setup
+app.use(bodyParser.json({
+
+}))
 
 
 // main route 
-app.get("/", function(req, res) {
-    res.send('Welcome to todays To-Do Planning!');
+app.get("/", (req, res) => {
+    res.send('Welcome to Todays To-Do Planning!');
 
 });
 
-app.listen(3000, function(){
-    console.log('Server Is Running Succesfuly ');
+app.listen(3000, () => console.log('Server Is Running Succesfuly '))
 
-});
+
 
 
