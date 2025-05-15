47ad5fd586da460a081d7d2e4b006f5180afdb1e
TMkPRINCIPE <princetmk10@gmail.com>
2025-05-15 03:13:16 -0700

    Created routes for sorting task by dates, status, and keyword. Also created a new file tasks.js for localy testing the different tasks without having need to connec to the database

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ console.log("Mongo URI from .env:", process.env.MONGO_URI);
 
 const dbURI = process.env.MONGO_URI;
 
+/*
 // Connect to MongoDB
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Failed to connect to MongoDB', err));
 
-
+*/ 
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
@@ -21,9 +22,13 @@ import router from './routes/index.js';
 import taskrouter from './routes/task.js';  
 
 
+// for testing searchint by dates, keywords and status localy before connecting to db
+import todoRouter from './routes/toDo.js';
+import indexRouter from './routes/index.js'  // home page route?
 
 
 const app = express();
+const PORT = 3000;
 
 // body-parser middleware setup
 //Incomming requests
@@ -35,48 +40,22 @@ app.use(bodyParser.urlencoded({
 
 }));
 
-//using toDo routes
-app.use('/tasks', taskrouter);
 
-
-//Public static files middleware
+// ## Public static files middleware
 app.use(express.static('public'));
 
-//Logging middleware using morgan for custom token creation
-
+// ## Logging middleware using morgan for custom token creation
 app.use(morgan('dev'));
 
-//views
-app.set('view engine', 'ejs');
 
-//keeping user cookies session setup
-app.use(session({
-    secret: 'Secrete key for individual user',
-    resave: false,
-    saveUninitialized: false
-}));
-
-// main route 
-app.use("/", router);
-
-//starting the server on port 3000 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
-
-
-
-app.listen(3000, () => console.log('Server Is Running Succesfully '))
-
-//using toDo routes
+/*
+// ## using toDo routes
 app.use('/task', taskrouter);
+*/
 
-
-//Public static files middleware
+// ## Public static files middleware
 app.use(express.static('public'));
 
-//Logging middleware using morgan for custom token cre
-
 //views
 app.set('view engine', 'ejs');
 
@@ -87,11 +66,14 @@ app.use(session({
     saveUninitialized: false
 }));
 
-// main route 
+// All routes here please 
 app.use("/", router);
+// app.use("/task", todoRouter);
+
+
 
 //starting the server on port 3000 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
--- routes/toDo.js
+++ routes/toDo.js
@@ -1,3 +1,30 @@
 // used to handle update, delete, add
 // used to keep app.js clean and help in management as app.js becomes more complex
 
+import express from 'express';
+import { getTasks } from '../models/tasks.js';
+
+
+const router = express.Router();
+//let tasks = []; // Lets create the tasks in the models folder. Thats the right approach when structuring our folder to content the userModels or anyother model. Infact we use it to create our different objects
+let count = 1;
+// app.use(express.json());
+
+
+router.get('/', (req, res) => {
+
+    try {   // Please guys let's us try using the try catch blocks to be able to easily notice our errors, if not we may take time debugging cudoe
+        
+        const tasks = getTasks(req.query);
+        res.json(tasks);
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+
+
+ 
+
+export default router;
